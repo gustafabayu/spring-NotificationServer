4dcc344bc518564f130baf3d0ec870855ef3394f
Gustafa Bayu <faafa13@gmail.com>
2023-10-15 16:15:17 +0700

    notification with kafka

---
+++ src/test/java/com/api/notificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.notificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
